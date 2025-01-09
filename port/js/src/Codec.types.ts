999d7da69193326ba8029ad6d76455c8a0879cf0
Dmitry Khitrin <dmitry.khitrin@alberblanc.com>
2025-01-09 22:40:27 +0200

    feat: parse type by type name

--- port/js/src/Codec.types.ts
+++ port/js/src/Codec.types.ts
@@ -3,6 +3,7 @@ import type { MessageId, ProtocolId } from './types';
 
 export type ProtocolMap = Map<ProtocolId, Map<MessageId, Converter>>;
 export type TypeMap = Map<MessageId, Converter>;
+export type TypeByName = Map<string, Converter>;
 
 export type ExtractPayload<
   Schema extends unknown,
