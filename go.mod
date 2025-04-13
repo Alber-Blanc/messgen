4b588be9dd2355907ef3cb46e79185a602fcacff
Philipp Andronov <filipp.andronov@gmail.com>
2025-04-13 14:54:51 +0300

    Go runtime support

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Alber-Blanc/messgen
+
+go 1.24.0
+
+replace (
+	github.com/Alber-Blanc/messgen => ./port/go/messgen
+)
---
+++ port/go/messgen/protocol.go
@@ -0,0 +1,5 @@
+package messgen
+
+// ProtocolId is unique identifier or a some protocol.
+// Ids must be unique across all protocols used in some particular system.
+type ProtocolId uint8
