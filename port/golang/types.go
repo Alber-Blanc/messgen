a16360a8bda1aa71a3a73ea7220df1ff3eb93deb
Philipp Andronov <filipp.andronov@gmail.com>
2025-05-05 16:30:59 +0300

    add common part

---
+++ port/golang/types.go
@@ -0,0 +1,24 @@
+package messgen
+
+import "fmt"
+
+type ProtocolId int16
+
+func (i ProtocolId) Int16() int16 {
+	return int16(i)
+}
+
+type MessageId int16
+
+func (i MessageId) Int16() int16 {
+	return int16(i)
+}
+
+type PayloadId struct {
+	Protocol ProtocolId
+	Message  MessageId
+}
+
+func (id PayloadId) String() string {
+	return fmt.Sprintf("%d:%d", id.Protocol, id.Message)
+}
