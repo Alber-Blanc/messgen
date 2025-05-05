cab11ccc6e16681be4ec875f32a84b2aef1fbba1
Philipp Andronov <filipp.andronov@gmail.com>
2025-05-05 16:30:59 +0300

    add common part

---
+++ port/golang/interfaces.go
@@ -0,0 +1,13 @@
+package messgen
+
+type Serializable interface {
+	Serialize(output []byte) (uint32, error)
+	Deserialize(input []byte) (uint32, error)
+	SerializedSize() uint32
+}
+
+type ProtoMessage interface {
+	Id() PayloadId
+	Hash() uint64
+	Data() Serializable
+}
