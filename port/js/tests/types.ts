7ecede7e90e6d7d025427294e9da1cd3a4858a2e
Dmitry Khitrin <dmitry.khitrin@alberblanc.com>
2025-10-13 15:13:21 +0300

    Do not store generated protocols and types files

--- port/js/tests/types.ts
+++
@@ -1,4 +0,0 @@
-import type * as MynamespaceTypes from './mynamespace/types';
-import type * as MynamespaceTypesSubspace from './mynamespace/types/subspace';
-
-export type TypeMap = MynamespaceTypes.TypeMap & MynamespaceTypesSubspace.TypeMap;
